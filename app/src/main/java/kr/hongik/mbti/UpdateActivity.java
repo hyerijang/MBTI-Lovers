5c9b1fbad88d794a2eb6d420546e6efef6d4568f
chjang5621 <chjang5621@naver.com>
2020-11-17 18:35:47 +0900

    fixed issue #2

--- app/src/main/java/kr/hongik/mbti/UpdateActivity.java
+++ app/src/main/java/kr/hongik/mbti/UpdateActivity.java
@@ -1,18 +1,24 @@
 package kr.hongik.mbti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UpdateActivity extends AppCompatActivity {
@@ -25,7 +31,10 @@ public class UpdateActivity extends AppCompatActivity {
     Uri profileImageUri;
     private final int GET_GALLERY_IMAGE = 200;
 
-
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference usersRef = db.collection("users").document(user.getUid());
+    EditText u_nickname, u_age, u_mbti, u_address ,u_stateMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +52,14 @@ public class UpdateActivity extends AppCompatActivity {
             }
         });
 
+
         btn_updateButton = findViewById(R.id.btn_updateButton);
 
-        iv_profile = (ImageView)findViewById(R.id.ImageForUpdate);
-        profileImage = new ProfileImage();
-        profileImage.showProfileImage(iv_profile);
+        u_nickname = (EditText) findViewById(R.id.u_nickname);
+        u_age = (EditText) findViewById(R.id.u_age);
+        u_mbti = (EditText) findViewById(R.id.u_mbti);
+        u_address = (EditText) findViewById(R.id.u_address);
+        u_stateMessage = (EditText) findViewById(R.id.u_stateMessage);
 
         btn_updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +69,12 @@ public class UpdateActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        getCurrentProfile();
+        iv_profile = (ImageView)findViewById(R.id.ImageForUpdate);
+        profileImage = new ProfileImage();
+        profileImage.showProfileImage(iv_profile);
 
         //iv_profile 클릭시 프로필이미지 재업로드 가능
         iv_profile.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +87,7 @@ public class UpdateActivity extends AppCompatActivity {
         });
 
     }
+
 
     private void profileUpdate2(){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -98,6 +117,35 @@ public class UpdateActivity extends AppCompatActivity {
             profileImageUri = data.getData();
             iv_profile.setImageURI(profileImageUri);
         }
+    }
+
+
+
+    /**
+     * document로부터 현재 프로필 정보를 가져오는 함수입니다.
+     **/
+    private void getCurrentProfile(){
+        usersRef.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                u_nickname.setText(document.getString("nickname"));
+                                u_age.setText(document.getString("age"));
+                                u_mbti.setText(document.getString("mbti"));
+                                u_address.setText(document.getString("address"));
+                                u_stateMessage.setText(document.getString("stateMessage"));
+                            } else {
+                                Log.d(TAG, "getCurrentProfile: No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "getCurrentProfile: failed to get document");
+                        }
+                    }
+                });
+
     }
 
 
