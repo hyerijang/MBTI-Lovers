3552fd5cc48eb884f65e2606914950b33e1b00f9
bhlee <djbh123@naver.com>
2020-11-20 15:02:35 +0900

    MyprofileActivity에서 뒤로가기 버튼 이벤트 추가

--- app/src/main/java/kr/hongik/mbti/MyprofileActivity.java
+++ app/src/main/java/kr/hongik/mbti/MyprofileActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,6 +33,14 @@ public class MyprofileActivity extends AppCompatActivity {
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     DocumentReference usersRef = db.collection("users").document(user.getUid());
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            myStartActivity(MainActivity.class);
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
