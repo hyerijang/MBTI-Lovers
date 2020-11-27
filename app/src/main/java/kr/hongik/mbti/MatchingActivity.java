d39403dfd00834035a27b9899f02f05e9d7f4682
bhlee <djbh123@naver.com>
2020-11-27 17:53:58 +0900

    구글 지도 MatchingActivity에 만듬

--- app/src/main/java/kr/hongik/mbti/MatchingActivity.java
+++ app/src/main/java/kr/hongik/mbti/MatchingActivity.java
@@ -1,12 +1,33 @@
 package kr.hongik.mbti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
-public class MatchingActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MatchingActivity extends AppCompatActivity implements AutoPermissionsListener {
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event){
@@ -20,7 +41,98 @@ public class MatchingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map=googleMap;
+            }
+        });
+
+        try{
+            MapsInitializer.initialize(this);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLocationService();
+            }
+        });
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
     }
+
+    public void startLocationService(){//위치 관리자 객체 참조
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(location!=null){
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000;
+            float minDistance=0;
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {//위치 리스너 구현
+        public void onLocationChanged(Location location){
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            showCurrentLocation(latitude, longitude);
+        }
+
+        private void showCurrentLocation(Double latitude, Double longitude){
+            LatLng curPoint = new LatLng(latitude, longitude);
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onDenied(int requestCode, @NotNull String[] permissions){
+
+    }
+
+    @Override
+    public void onGranted(int requestCode, @NotNull String[] permissions){
+
+    }
+
 
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
