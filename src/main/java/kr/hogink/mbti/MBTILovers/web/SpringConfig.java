1e2f0a6fb79fc12e5b21478cc9f265207df000b8
Hyeri Jang <chjang5621@naver.com>
2021-03-01 19:07:06 +0900

    JDBC Template 리포지토리 구현

--- src/main/java/kr/hogink/mbti/MBTILovers/web/SpringConfig.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/SpringConfig.java
@@ -2,11 +2,13 @@ package kr.hogink.mbti.MBTILovers.web;
 
 
 import kr.hogink.mbti.MBTILovers.web.repository.JdbcMemberRepository;
+import kr.hogink.mbti.MBTILovers.web.repository.JdbcTemplateMemberRepository;
 import kr.hogink.mbti.MBTILovers.web.repository.MemberRepository;
 import kr.hogink.mbti.MBTILovers.web.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -26,6 +28,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
