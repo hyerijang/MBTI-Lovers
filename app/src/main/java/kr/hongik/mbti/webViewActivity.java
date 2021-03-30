7ea0e11d28568d7bb22f61fd43e3be586c6cec4c
Hyeri Jang <chjang5621@naver.com>
2021-03-30 20:20:39 +0900

    웹뷰 설정:  자바스크립트 허용 등

--- app/src/main/java/kr/hongik/mbti/webViewActivity.java
+++ app/src/main/java/kr/hongik/mbti/webViewActivity.java
@@ -1,23 +1,20 @@
 package kr.hongik.mbti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import org.apache.http.cookie.Cookie;
-
-import java.util.List;
-
 public class webViewActivity extends Activity {
 
-    private WebView webView;
-    private WebSettings mWebSettings; //웹뷰세팅
+    private WebView wv;
+    private WebSettings settings; //웹뷰세팅
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +23,12 @@ public class webViewActivity extends Activity {
         Intent intent = getIntent();
 
         String myUrlAddress = intent.getStringExtra("myurl");
-        webView = findViewById(R.id.wv);
-        webView.setWebViewClient(new WebViewClient());
+        wv = findViewById(R.id.wv);
+        wv.setWebViewClient(new WebViewClient());
+        wv.setWebChromeClient(new WebChromeClient()); //콜백 허용
 
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-        List<Cookie> cookies = SessionControl.cookies;
-        cookieManager.removeAllCookie();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                String cookieString = cookie.getName() + "=" + cookie.getValue() + "; Domain=" + cookie.getDomain();
-                cookieManager.setCookie(cookie.getDomain(), cookieString);
-
-            }
-        }
-        webView.loadUrl(myUrlAddress);
+        setWebView();
+        wv.loadUrl(myUrlAddress);
     }
 
     public void onResume() {
@@ -53,20 +41,18 @@ public class webViewActivity extends Activity {
 
     }
 
-    public void setWebView(){
-        mWebSettings = webView.getSettings(); //세부 세팅 등록
-        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
-        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
-        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
-        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
-        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
-        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
-        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
-        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
-        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
-
-
+    public void setWebView() {
+        settings = wv.getSettings(); //세부 세팅 등록
+        settings.setJavaScriptEnabled(true); // 웹페이지 자바스크립트 허용 여부
+        settings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+        settings.setJavaScriptCanOpenWindowsAutomatically(true); //팝업창 허영
+        settings.setLoadsImagesAutomatically(true); //웹뷰가 앱의 이미지 리소스 로드
+//        settings.setUseWideViewPort(true); //wide viewport 사용
+        settings.setSupportZoom(false); //zoom 비허용
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE); //캐시 비허용 : 웹뷰 속도 향상
+        settings.setAppCacheEnabled(false); //앱 내부 캐시 사용 안함
+        settings.setDomStorageEnabled(true); //로컬스토리지 사용 허용
+        settings.setAllowFileAccess(true);//웹뷰에서 파일 액세스 활성화
     }
 }
 
