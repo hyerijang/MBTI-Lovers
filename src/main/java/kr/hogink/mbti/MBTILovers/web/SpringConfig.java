e6ac21fe6b6e0819839a6a62c11bccea5ff35fdb
Hyeri Jang <chjang5621@naver.com>
2021-04-09 21:46:18 +0900

    [Done] WebConfig 분리
    - Interceptors
    - ResourceHandlers

--- src/main/java/kr/hogink/mbti/MBTILovers/web/SpringConfig.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/SpringConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class SpringConfig extends WebMvcConfigurationSupport {
+public class SpringConfig  {
     private final MemberRepository memberRepository;
     private final FriendRepository friendRepository;
 
@@ -34,37 +34,12 @@ public class SpringConfig extends WebMvcConfigurationSupport {
     public FriendService friendService() {
         return new FriendServiceImpl(friendRepository);
     }
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }
-
-
-    @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        // 인증 인터셉터
-        // 로그인된 유저인지 검증
-        // 경로는 "/경로" 여야함
-        registry.addInterceptor(new AuthInterceptor(memberService()))
-                .addPathPatterns("/*");
-
-        //로그인 인터셉터
-        //로그인을 처리함
-        registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/user/loginPost");
-
-
-    }
-
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/"
-    };
-
 
     @Bean
     public LoginService loginService() {
         return new LoginServiceImpl(memberService());
     }
+
+
+
 }
---
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/WebConfig.java
@@ -0,0 +1,43 @@
+package kr.hogink.mbti.MBTILovers.web;
+
+import kr.hogink.mbti.MBTILovers.web.login.AuthInterceptor;
+import kr.hogink.mbti.MBTILovers.web.login.LoginInterceptor;
+import kr.hogink.mbti.MBTILovers.web.member.MemberService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    private  final MemberService memberService;
+
+    public WebConfig(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        // 인증 인터셉터
+        // 로그인된 유저인지 검증
+        // 경로는 "/경로" 여야함
+        registry.addInterceptor(new AuthInterceptor(memberService))
+                .addPathPatterns("/*");
+
+        //로그인 인터셉터
+        //로그인을 처리함
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/user/loginPost");
+
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/", "classpath:/templates/"
+    };
+}
