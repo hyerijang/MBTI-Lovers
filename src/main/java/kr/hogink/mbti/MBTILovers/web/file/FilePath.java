ebfd1c70af31eff189d70c3a5d68e4bcfab23712
Hyeri Jang <chjang5621@naver.com>
2021-04-21 15:52:55 +0900

    refector :사용하지 않는 File 클래스 삭제
    Resolves  : #28

--- src/main/java/kr/hogink/mbti/MBTILovers/web/file/FilePath.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/file/FilePath.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.util.regex.Matcher;
 
 public class FilePath {
-    //    File file = new File("");
-    //    private final String projectPath = file.getAbsolutePath();
     private static final String projectPath = System.getProperty("user.dir");
     private static final String additionalPath = "/src/main/resources/static/images";
     static final String imagesPath = projectPath + additionalPath.replaceAll("/", Matcher.quoteReplacement(File.separator));
