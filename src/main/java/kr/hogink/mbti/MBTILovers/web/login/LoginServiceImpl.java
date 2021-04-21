8362d6f60f54c5e43bccdb88d96557d96aa8dc04
Hyeri Jang <chjang5621@naver.com>
2021-04-21 17:00:26 +0900

    refector : AppConfig를 컴포넌트 스캔 방식으로 변경
    Resolves  : #29

--- src/main/java/kr/hogink/mbti/MBTILovers/web/login/LoginServiceImpl.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/login/LoginServiceImpl.java
@@ -2,13 +2,16 @@ package kr.hogink.mbti.MBTILovers.web.login;
 
 import kr.hogink.mbti.MBTILovers.web.member.Member;
 import kr.hogink.mbti.MBTILovers.web.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class LoginServiceImpl implements LoginService {
 
     private final MemberService memberService;
-
+    
     public LoginServiceImpl(MemberService memberService) {
         this.memberService = memberService;
     }
