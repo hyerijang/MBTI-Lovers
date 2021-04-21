2c97c1d63f026de0a1bcbb3cd4d8f188a6dafefe
Hyeri Jang <chjang5621@naver.com>
2021-04-21 16:07:26 +0900

    refector : SpringConfig를 AppConfig로 이름 변경

---
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/AppConfig.java
@@ -0,0 +1,57 @@
+package kr.hogink.mbti.MBTILovers.web;
+
+
+import kr.hogink.mbti.MBTILovers.web.chat.room.RoomRepository;
+import kr.hogink.mbti.MBTILovers.web.chat.room.RoomService;
+import kr.hogink.mbti.MBTILovers.web.chat.room.RoomServiceImpl;
+import kr.hogink.mbti.MBTILovers.web.file.FileService;
+import kr.hogink.mbti.MBTILovers.web.friend.FriendRepository;
+import kr.hogink.mbti.MBTILovers.web.friend.FriendService;
+import kr.hogink.mbti.MBTILovers.web.friend.FriendServiceImpl;
+import kr.hogink.mbti.MBTILovers.web.login.LoginService;
+import kr.hogink.mbti.MBTILovers.web.login.LoginServiceImpl;
+import kr.hogink.mbti.MBTILovers.web.member.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    private final MemberRepository memberRepository;
+    private final FriendRepository friendRepository;
+    private final RoomRepository roomRepository;
+
+    public AppConfig(MemberRepository memberRepository, FriendRepository friendRepository, RoomRepository roomRepository) {
+        this.memberRepository = memberRepository;
+        this.friendRepository = friendRepository;
+        this.roomRepository = roomRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository);
+    }
+
+    @Bean
+    public FriendService friendService() {
+        return new FriendServiceImpl(friendRepository, memberRepository);
+    }
+
+    @Bean
+    public LoginService loginService() {
+        return new LoginServiceImpl(memberService());
+    }
+
+
+    @Bean
+    public RoomService roomService() {
+        return new RoomServiceImpl(roomRepository);
+    }
+
+//    @Bean
+//    public FileService fileService() {
+//        return new FileService();
+//    }
+
+
+}
+
