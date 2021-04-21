4f93973368b744c51ee04df5eed6b8a2568aaebd
Hyeri Jang <chjang5621@naver.com>
2021-04-21 15:11:50 +0900

    chore : 메세지 컨트롤러에서 room정보 갱신하는 함수 따로 추출

--- src/main/java/kr/hogink/mbti/MBTILovers/web/chat/message/MessageController.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/chat/message/MessageController.java
@@ -2,6 +2,7 @@ package kr.hogink.mbti.MBTILovers.web.chat.message;
 
 import kr.hogink.mbti.MBTILovers.web.chat.room.Room;
 import kr.hogink.mbti.MBTILovers.web.chat.room.RoomService;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -25,9 +26,12 @@ public class MessageController {
     @MessageMapping("/chat.sendMessage") //기존 request mapping 역할
     public void sendMessage(@Payload Message chatMessage) {
         Long rid = chatMessage.getRid();
-        System.out.println("현재 방 : " + rid);
         messagingTemplate.convertAndSend("/sub/chat/room/".trim() + rid, chatMessage);
+        renewRoominfo(chatMessage, rid);
+    }
 
+    //최신 메세지 정보 저장
+    private void renewRoominfo(Message chatMessage, Long rid) {
         Room room = roomService.findRoomByRid(rid);
         room.setLastSentContent(chatMessage.getContent());
         room.setLastSentTimeAt(chatMessage.getSentTimeAt());
