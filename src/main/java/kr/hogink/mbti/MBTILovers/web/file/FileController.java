0a67bd3ebefc86db2f08bace215719c7e0552df5
Hyeri Jang <chjang5621@naver.com>
2021-04-24 01:00:29 +0900

    feat : 프로필 이미지 서버 저장시 파일명 uid + 확장자
    서버에 프로필 이미지 저장시  파일명은 uid + 확장자
    Resolves  : #28

--- src/main/java/kr/hogink/mbti/MBTILovers/web/file/FileController.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/file/FileController.java
@@ -1,21 +1,27 @@
 package kr.hogink.mbti.MBTILovers.web.file;
 
+import kr.hogink.mbti.MBTILovers.web.login.LoginService;
+import kr.hogink.mbti.MBTILovers.web.login.LoginType;
+import kr.hogink.mbti.MBTILovers.web.member.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@SessionAttributes({LoginType.USER_MEMBER_SESSION, LoginType.NEW_USER_UID_SESSION})
 @Controller
 public class FileController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     FileService fileService;
+    LoginService loginService;
 
     public FileController(FileService fileService) {
         this.fileService = fileService;
@@ -36,5 +42,22 @@ public class FileController {
         return "redirect:/";
 
     }
+
+    @PostMapping("/uploadProfileImage")
+    public String uploadProfileImage(@RequestParam("file") MultipartFile file, @ModelAttribute(LoginType.USER_MEMBER_SESSION) Member user, @ModelAttribute(LoginType.USER_MEMBER_SESSION) Member new_user) {
+        logger.info("--------------------------------------");
+        logger.info("Upload File Name : " + file.getOriginalFilename());
+        logger.info("Upload File Size : " + file.getSize());
+        String uid;
+        if (user == null) {
+            uid = new_user.getUid();
+        } else
+            uid = user.getUid();
+
+        fileService.fileUpload(file, uid);
+        return "redirect:/";
+
+    }
+
 
 }
