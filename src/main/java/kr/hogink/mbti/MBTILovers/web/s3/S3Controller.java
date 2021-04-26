e50c6c66d270ca84c3609b0dfea576f2e2961f6c
Hyeri Jang <chjang5621@naver.com>
2021-04-26 18:22:45 +0900

    feat : s3 이미지 업로드

---
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/s3/S3Controller.java
@@ -0,0 +1,29 @@
+package kr.hogink.mbti.MBTILovers.web.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Controller
+public class S3Controller {
+    private final S3Uploader s3Uploader;
+
+    @GetMapping("/test")
+    public String index() {
+        return "test";
+    }
+
+    @PostMapping("/uploadS3")
+    @ResponseBody
+    public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.upload(multipartFile, "static");
+    }
+}
+
