75f37a41d9ae5996c86a8cf976482e7208dec2e9
Hyeri Jang <chjang5621@naver.com>
2021-05-28 18:07:50 +0900

    chore : 홈 화면을 친구목록으로 변경
    기존 홈 화면은 마이페이지로 변경

--- src/main/java/kr/hogink/mbti/MBTILovers/web/HomeController.java
+++ src/main/java/kr/hogink/mbti/MBTILovers/web/HomeController.java
@@ -1,7 +1,11 @@
 package kr.hogink.mbti.MBTILovers.web;
 
+import kr.hogink.mbti.MBTILovers.web.friend.Friend;
+import kr.hogink.mbti.MBTILovers.web.friend.FriendController;
+import kr.hogink.mbti.MBTILovers.web.friend.FriendService;
 import kr.hogink.mbti.MBTILovers.web.login.LoginType;
 import kr.hogink.mbti.MBTILovers.web.member.Member;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +15,31 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @SessionAttributes(LoginType.USER_MEMBER_SESSION)
 @Log4j2
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final FriendService friendService;
+
     @GetMapping("/")
     public String home(Model model, @ModelAttribute(LoginType.USER_MEMBER_SESSION) Member member) {
+
+        List<Friend> friends = friendService.getListRelationFriend(member.getUid());
+        if (!friends.isEmpty()) {
+            Collections.sort(friends, FriendController::compare);
+        }
+        model.addAttribute("friends", friends);
+
+        return "friend/friendsList";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage(Model model, @ModelAttribute(LoginType.USER_MEMBER_SESSION) Member member) {
         if (member != null) {
             model.addAttribute("uid", member.getUid());
             model.addAttribute("user", member);
