13df2469fe9a4ad6933d0240ca62e7a3171b2984
Hyeri Jang <chjang5621@naver.com>
2021-06-14 19:54:39 +0900

    fix : 오류 수정
    - 차단 당했다 풀렸을 때 상대 유저가 매칭에 뜨지 않는 오류 수정
    - 차단 목록의 내비게이션 바에 친구목록이 disable로 되어있는 것 수정
    - firebase realtime 데이터베이스 딜레이 때문에 마지막 메세지가 아닌 메세지를 화면에 표시하는 오류 수정
    -->소켓 통신으로 수신한 메시지를  파이어베이스 거치지 않고  바로 읽도록 수정

--- server/src/main/java/kr/hogink/mbti/MBTILovers/web/member/MemberRepository.java
+++ server/src/main/java/kr/hogink/mbti/MBTILovers/web/member/MemberRepository.java
@@ -22,7 +22,7 @@ public interface MemberRepository {
     List<Member> findNearPoint(double latitude, double longitude, int n);
 
 
-    @Query(value = "SELECT r.*  FROM (SELECT m.* \tFROM member m \tleft join friend f \ton f.uid = ?4 and m.uid = f.fid where f.uid is null or f.relation not in (\"FRIEND\",\"BLOCK\"))  as r where r.location is not null order by ST_Distance(location, POINT(?1, ?2))  limit ?3", nativeQuery = true)
+    @Query(value = "SELECT r.*  FROM (SELECT m.* \tFROM member m \tleft join friend f \ton f.uid = ?4 and m.uid = f.fid where f.uid is null or f.relation not in (\"FRIEND\",\"BLOCK\",\"BLOCKED\"))  as r where r.location is not null order by ST_Distance(location, POINT(?1, ?2))  limit ?3", nativeQuery = true)
     List<Member> findNearPointNotFriend(double latitude, double longitude, int n, String uid);
 
     void deleteById(String s);
